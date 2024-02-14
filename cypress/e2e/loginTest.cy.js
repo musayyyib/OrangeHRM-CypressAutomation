87a25a8f8c19245af77b6af030eb80061334385a
musayyyib <musayyib.ahmed@outlook.com>
2024-02-14 15:12:23 +0500

    updated files

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -17,7 +17,7 @@ describe("Login Test - OrangeHRM", () => {
 
     const incorrectUsername = "admin", incorrectPassword = "admin1234"
 
-    it.only("Login - Negative Flow", () => {
+    it("Login - Negative Flow", () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
         cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").type(incorrectUsername)
         cy.get(":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input").type(incorrectPassword)
